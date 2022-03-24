97efdb3348935048daa754ed3b66a01e7e40f0a8
nasty1326 <92375864+nasty1326@users.noreply.github.com>
2022-03-24 11:57:41 +0700

    Add files via upload

---
+++ 931903.gorbatyuk.anastasiya.lab4/931903.gorbatyuk.anastasiya.lab4/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace _931903.gorbatyuk.anastasiya.lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.edToLive = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.edLive1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.edLive2 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.edToLive)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edLive1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edLive2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Старт/Стоп";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(26, 90);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 600);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(214, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Оживает, если рядом ";
+            // 
+            // edToLive
+            // 
+            this.edToLive.Location = new System.Drawing.Point(365, 7);
+            this.edToLive.Maximum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.edToLive.Name = "edToLive";
+            this.edToLive.Size = new System.Drawing.Size(41, 22);
+            this.edToLive.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(412, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "соседей";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(214, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Остается живой, если рядом ";
+            // 
+            // edLive1
+            // 
+            this.edLive1.Location = new System.Drawing.Point(415, 39);
+            this.edLive1.Maximum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.edLive1.Name = "edLive1";
+            this.edLive1.Size = new System.Drawing.Size(41, 22);
+            this.edLive1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(462, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "или";
+            // 
+            // edLive2
+            // 
+            this.edLive2.Location = new System.Drawing.Point(500, 39);
+            this.edLive2.Maximum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.edLive2.Name = "edLive2";
+            this.edLive2.Size = new System.Drawing.Size(41, 22);
+            this.edLive2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(547, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "соседей";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 718);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.edLive2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.edLive1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.edToLive);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.edToLive)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edLive1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edLive2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown edToLive;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown edLive1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown edLive2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ 931903.gorbatyuk.anastasiya.lab4/931903.gorbatyuk.anastasiya.lab4/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _931903.gorbatyuk.anastasiya.lab4
+{
+    public partial class Form1 : Form
+    {
+        int sizepanel_W, sizepanel_H; // размер панели
+        int sizecell_W, sizecell_H;// размер ячейки
+
+        const int cell_W = 30, cell_H = 30;//количество ячеек, далее номер 
+
+        
+        bool Proc; //процесс
+        Bitmap Bmap; //объект рисования пикселями
+        Graphics graphic; // поверхность рисования
+        bool[,] Cell_Mat;//клеточная матрицы
+        int toLive, live1, live2;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            One_Step();// создаем доску рисования и первое поколение
+            PaintingOverCell(); //закрашиваем ячейки первого поколения
+            DrawGridLines(); //Рисуем линии сетки
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            toLive = (int)edToLive.Value;
+            live1 = (int)edLive1.Value;
+            live2 = (int)edLive2.Value;
+            if (Proc)
+            {
+                timer1.Stop();
+            }
+            else
+            {
+                timer1.Start();
+            }
+            Proc = !Proc;
+        }
+
+        private void One_Step() // создаем доску рисования и первое поколение
+        {
+            sizepanel_W = panel1.Width;// считываем ширину панели
+            sizepanel_H = panel1.Height;//считываем высоту панели
+            sizecell_W = sizepanel_W / cell_W;//размер счейки равен размер панели по ширине/ количество ячеек
+            sizecell_H = sizepanel_H / cell_H;//размер счейки равен размер панели по высоте/ количество ячеек 
+
+            Bmap = new Bitmap(sizepanel_W, sizepanel_W);//задаем область рисования по размеру панели 
+            graphic = Graphics.FromImage(Bmap);// создаем новый объект рисования из bm
+
+            Cell_Mat = new bool[cell_H, cell_W];// создаем  тип ячейки с параметрами координат 
+
+
+            //Создаем начальное поколение (можно с рандомом)
+            Random rnd = new Random();
+            int n = rnd.Next(100, 400);
+          
+            for (int i=0;i<=n;i++)
+            {
+                Cell_Mat[rnd.Next(0, 29), rnd.Next(0, 29)] = true;
+            }
+        }
+
+        private void PaintingOverCell() //создаем кисть для заливки 
+        {
+            SolidBrush Brush = new SolidBrush(Color.Red); //создаем кисть заливки красного цвета
+
+            //Закрашиваем ячейку,если она true
+            for (int i = 0; i < cell_H; i++)
+            {
+                for (int j = 0; j <cell_W; j++)
+                {
+                    if (Cell_Mat[i, j])
+                    {
+                        graphic.FillRectangle(Brush, j * sizecell_W, i * sizecell_H, sizecell_W, sizecell_H);
+                    }
+                }
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(Bmap, Point.Empty);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            graphic.Clear(Color.White); //очищаем поверхность рисования
+            Next_Step();  // создаем следующее поколение
+            PaintingOverCell();//закрашиваем ячейки первого поколения
+            DrawGridLines(); //Рисуем линии сетки
+            panel1.Invalidate(); //перерисовываем панель
+        }
+
+        private void DrawGridLines() // Рисуем сетку
+        {
+            Pen pen = new Pen(Color.Black); //создаем кисть для рисования линий
+            //вертикальные линии
+            for (int i = 0; i < cell_W + 1; i++)
+            {
+                graphic.DrawLine(pen, i * sizecell_W, 0, i * sizecell_W, sizepanel_H);
+            }
+            // горизнтальные линии
+            for (int i = 0; i < cell_H + 1; i++)
+            {
+                graphic.DrawLine(pen, 0, i * sizecell_H, sizepanel_W, i * sizecell_H);
+            }
+        }
+
+        private void Next_Step() //создаем следующее поколение
+        {
+            bool[,] Next_Mat = new bool[cell_H, cell_W]; // создаем тип ячейки с параметрами координат следующего поколения
+
+            for (int i = 0; i < cell_H; i++)
+            {
+                for (int j = 0; j < cell_W; j++)
+                {
+                    if (Cell_Mat[i, j]) // если ячейка живая
+                    {
+                        int alive = NumberLivingNeighbors(i, j); // число ее соседей
+                        Next_Mat[i, j] = (alive == live2) || (alive == live1); // если 2 или 3 соседа, тогда жива
+                    }
+                    else //если ячейка мертвая
+                    {
+                        Next_Mat[i, j] = NumberLivingNeighbors(i, j) == toLive; // оживает, если 3 соседа рядом 
+                    }
+                }
+            }
+            Cell_Mat = Next_Mat; // переносим это поколение в массив старого поколения
+        }
+
+        private int NumberLivingNeighbors(int i, int j) // число живых пикселей вокруг  
+        {
+            int aliveNeighbours = 0; // число живых соседей
+            for (int x = -1; x <= 1; x++) // -1 0 1
+            {
+                int m = i + x; // по высоте сверху и снизу
+
+                if ((m >= 0) && (m < cell_H)) // если сосед в пределах сетки по высоте
+                    for (int y = -1; y <= 1; y++) // -1 0 1
+                    {
+                        int a = j + y;
+
+                        if ((a >= 0) && (a < cell_W)) // если сосед в пределах сетки по ширине
+                            aliveNeighbours += Cell_Mat[m, a] ? 1 : 0; // если сосед существует, прибавляем его
+                    }
+            }
+
+            aliveNeighbours -= Cell_Mat[i, j] ? 1 : 0; // вычитаем саму ячейку, если она есть
+
+            return aliveNeighbours; // возвращаем число соседей
+        }
+    }
+}
